ee8e13aea26625f1c30a2a94089664263078c6af
yunusyalcinkaya <yunusyalcinkaya52@gmail.com>
2023-05-29 19:35:51 +0300

    Initial commit

---
+++ filter-service/src/main/java/com/kodlamaio/filterservice/api/controllers/FiltersController.java
@@ -0,0 +1,33 @@
+package com.kodlamaio.filterservice.api.controllers;
+
+import com.kodlamaio.commonpackage.utils.dto.ChangeCarStateRequest;
+import com.kodlamaio.filterservice.business.abstracts.FilterService;
+import com.kodlamaio.filterservice.business.dto.responses.GetAllFiltersResponse;
+import com.kodlamaio.filterservice.business.dto.responses.GetFilterResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/filters")
+public class FiltersController {
+    private final FilterService service;
+
+    @GetMapping
+    public List<GetAllFiltersResponse> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public GetFilterResponse getByIId(@PathVariable UUID id) {
+        return service.getById(id);
+    }
+
+    @PutMapping("/change-car-state")
+    public void changeCarState(@RequestBody ChangeCarStateRequest request) {
+        service.changeStateByCarId(request.getCarId(), request.getCarState());
+    }
+}
